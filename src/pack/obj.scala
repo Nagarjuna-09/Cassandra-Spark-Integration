81f3cdce59aea9ba38803acdbfbad163b141eca6
Nagarjuna-09 <nagarjunanathani434@gmail.com>
2024-01-27 11:55:26 -0500

    cassandra read & write code included

--- src/pack/obj.scala
+++ src/pack/obj.scala
@@ -20,7 +20,32 @@ object obj {
     
     val spark = SparkSession.builder().getOrCreate( )
     import spark.implicits._
-
+    
+    val df = spark.read
+                 .format("org.apache.spark.sql.cassandra")
+                 .option("spark.cassandra.connection.host","localhost") //running on local host
+                 .option("spark.cassandra.connection.port","9042") //9042 is default port on which cassandra runs
+                 .option("keyspace","b36") //keyspace name
+                 .option("table","ztab") //table name
+                 .load()
+                 
+    df.show()    
+    
+    
+    //processing cassandra table
+    val finaldf = df.filter(col("id")>1)
+    finaldf.show()
+    
+    
+    //writing to a different cassandra table (imagine this cassandra is running in a different host in real time)
+    finaldf.write
+         .format("org.apache.spark.sql.cassandra")
+         .option("spark.cassandra.connection.host","localhost") //running on local host
+         .option("spark.cassandra.connection.port","9042") //9042 is default port on which cassandra runs
+         .option("keyspace","b361") //This keyspace should already exist in the destination cassandra
+         .option("table","zout") // this table should already exist in the destination cassandra keyspace
+         .save()
+    
 
     
   }
